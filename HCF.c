d11b023618b322648bcd7ca7c7e4e9ec1de07fc5
Katalyst99 <katleho333@gmail.com>
2024-10-11 12:52:29 -0700

    Highest common factor

--- HCF.c
+++ HCF.c
@@ -1,18 +1,47 @@
 #include <stdio.h>
 
+/*
+ * hcf - highest common factor
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The HCF of a and b.
+ */
+
 int hcf(int a, int b)
 {
-	int tmp;
+    int tmp;
 
-	while (a > 0)
-	{
-		if (a < b)
-		{
-			tmp = a;
-			a = b;
-			b = tmp;
-		}
-		a = a - b;
-	}
-	return b:
+    while (a > 0)
+    {
+        if (a < b)
+        {
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+        a = a - b;
+    }
+    return b;
 }
+
+int main()
+{
+    int x, y;
+
+    printf("Enter two positive integers (space-separated):\n");
+
+    while (scanf("%d %d", &x, &y) != EOF)
+    {
+        if (x > 0 && y > 0)
+        {
+            printf("HCF of %d and %d is %d\n", x, y, hcf(x, y));
+        }
+        else
+        {
+            printf("Please enter positive integers.\n");
+        }
+    }
+    return (0);
+}
+
